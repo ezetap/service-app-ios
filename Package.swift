d00b9d99b2d75186d9ffba9c54153e220c252d03
Atif Qamar <atif.q@ezetap.com>
2025-06-30 10:47:40 +0530

    Added PostHog dependency version 3.7.2

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/PostHog/posthog-ios.git", .exact("3.7.2")),
     ],
     targets: [
         
